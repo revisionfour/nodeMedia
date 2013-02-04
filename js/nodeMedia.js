999726f065366354e63b159bf7bbbbdf7f1533ea
OpenSourceAnonymous <chris.lee@qrhook.com>
2013-02-04 00:47:30 -0500

    Got audio working

---
+++ js/nodeMedia.js
@@ -0,0 +1,17 @@
+// nodeMedia.js
+
+var Main = {
+	init:function(){
+		player.src = "http://631b97b9d9ef7dc1f42d-870e834b13cbfa78e888d3ce02482563.r87.cf1.rackcdn.com/07%20-%20Stay%20Crunchy.mp3";
+		player.play();
+	}
+}
+
+var player = document.getElementById('mediaPlayer');
+
+Main.init();
+
+
+
+
+
